4d7a2e04ec9d848614f0d0a7912eb357202e0135
N E I L  O H E N E <54632119+dvc-77@users.noreply.github.com>
2024-03-26 19:04:40 +0000

    feat: added runner class with cli integration

--- src/main/java/com/boney/qrcode/QRCodeGenerator.java
+++ src/main/java/com/boney/qrcode/QRCodeGenerator.java
@@ -16,13 +16,27 @@ import java.util.Map;
 
 public class QRCodeGenerator {
     private File outputFile;
+    private String text;
+    private String format;
+    private int size;
 
-    public void generateQrCode(String text, String format, int size) throws WriterException, IOException {
+    public QRCodeGenerator(String text, String format, int size) {
+        this.text = text;
+        this.format = format;
+        this.size = size;
+    }
+
+    public void generateQrCode() throws WriterException, IOException {
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
         Map<EncodeHintType, ErrorCorrectionLevel> hints = new EnumMap<>(EncodeHintType.class);
         hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
 
         BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, size, size, hints);
         MatrixToImageWriter.writeToPath(bitMatrix, format, outputFile.toPath());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("\tText: %s\n\tFormat: %s\n\tSize: %d",text, format, size);
     }
 }
--- src/main/java/com/boney/qrcode/RunnerClass.java
+++ src/main/java/com/boney/qrcode/RunnerClass.java
@@ -1,10 +1,14 @@
 package com.boney.qrcode;
 
+import com.google.zxing.WriterException;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.io.IOError;
+import java.io.IOException;
+import java.io.WriteAbortedException;
 import java.util.concurrent.Callable;
 
 @Command(name = "bqr",
@@ -12,6 +16,8 @@ import java.util.concurrent.Callable;
         mixinStandardHelpOptions = true,
         header = "Simple cli tool to create a QR code and render QR code to text.")
 public class RunnerClass implements Callable<Integer> {
+        @Option(names = {"-v", "--verbose"}, description = "More verbose output")
+        private boolean verbose = false;
 
         @Option(names = { "-s", "--size" }, description = "Image size")
         String size;
@@ -27,7 +33,27 @@ public class RunnerClass implements Callable<Integer> {
 
 
         @Override
-        public void call() {
+        public Integer call() throws WriterException, IOException {
+                System.out.println("[+] Generating QR code.....");
+                QRCodeGenerator qrCodeGenerator = new QRCodeGenerator(text, format, Integer.parseInt(size));
+
+                if (verbose) {
+                        System.out.println("[.] Params:");
+                        System.out.println(qrCodeGenerator);
+                }
+
+                try {
+                        qrCodeGenerator.generateQrCode();
+                        System.out.println("[+] QR code generated. Output: " + outputFile);
+                        return 0;
+                } catch (WriterException e) {
+                        throw new WriterException("[-] Error while generating QR code (" + e.getMessage() + ")");
+                        return 1;
+                } catch (IOException e) {
+                        throw new IOException("[-] Error while generating file (" + e.getMessage() + ")");
+                        return 2;
+                }
+
 
         }
 
